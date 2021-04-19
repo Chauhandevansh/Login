002aa431dffc24043e02b20c8c0dd63808ddb03e
Chauhandevansh <chauhandevansh04@gmail.com>
2021-04-19 13:38:07 +0530

    hashing

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path  = require("path");
+const path = require("path");
 const app = express();
 const hbs = require("hbs");
 require("./db/conn");
@@ -12,71 +12,91 @@ const template_path = path.join(__dirname, "../templates//views");
 const partials_path = path.join(__dirname, "../templates/partials");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(static_path));
-app.set("view engine","hbs");
-app.set("views",template_path);
+app.set("view engine", "hbs");
+app.set("views", template_path);
 hbs.registerPartials(partials_path);
 
-app.get("/",(req, res) => {
+app.get("/", (req, res) => {
     res.render("index")
 });
-app.get("/register", (req, res) =>{
+app.get("/register", (req, res) => {
     res.render("index")
 });
-app.get("/login", (req, res) =>{
+app.get("/login", (req, res) => {
     res.render("index")
 });
-app.post("/register", async (req, res) =>{
-    try{
-      const password = req.body.password;
-      const cpassword = req.body.confirmpassword;
+app.post("/register", async (req, res) => {
+    try {
+        const password = req.body.password;
+        const cpassword = req.body.confirmpassword;
 
-      if(password === cpassword){
+        if (password === cpassword) {
 
-        const registerUser = new Resgister({
-            userid : req.body.userid,
-            email : req.body.email,
-            phone : req.body.phone,
-            password : password,
-            confirmpassword : cpassword 
-        })
-        const registered = await registerUser.save();
-        res.status(201).render("index");
+            const registerUser = new Resgister({
+                userid: req.body.userid,
+                email: req.body.email,
+                phone: req.body.phone,
+                password: password,
+                confirmpassword: cpassword
+            })
 
-      }else{
-          res.send("password are not matching")
-      }
-     
-    }catch (error) {
+
+
+            const registered = await registerUser.save();
+            res.status(201).render("index");
+
+        } else {
+            res.send("password are not matching")
+        }
+
+    } catch (error) {
         res.status(400).send(error);
     }
-    
+
 })
 //login check
-app.post("/login", async(req, res) =>{
-    try{
+app.post("/login", async (req, res) => {
+    try {
         const email = req.body.email;
         const password = req.body.password;
 
-        const useremail = await Resgister.findOne({email:email});
-        
-        if(useremail.password === password){
+        const useremail = await Resgister.findOne({ email: email });
+
+        if (useremail.password === password) {
             res.status(201).render("home");
-        }else{
-            res.send("password are not matching")
+        } else {
+            res.render("login");
+            alert("incorrect password");
         }
-        
-        
+
+
     } catch (error) {
         res.status(400).send("invalid email")
     }
 
 
 })
-    
+//const bcrypt = require("bcryptjs");
+//const securePassword = async (password) =>{
+//   const passwordHash = await bcrypt.hash(password, 10);
+//    console.log(passwordHash);
 
-app.listen(port, () =>  {
-    console.log(`server runing at port no ${port}` );
+//   const passwordmatch = await bcrypt.compare("devr@123", passwordHash);
+//   console.log(passwordmatch);
+
+
+//}
+//securePassword("devr@123");
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`server runing at port no ${port}`);
 })
--- mernbackend/src/models/registers.js
+++ mernbackend/src/models/registers.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const brypt = require("bcryptjs");
 const regSchema = new mongoose.Schema({
     userid : {
         type:String,
@@ -23,6 +24,20 @@ const regSchema = new mongoose.Schema({
         required:true
     }
 })
+
+regSchema.pre("save", async function(next) {
+    if(this.isModified("password")){
+        //const passwordHash = await bcrypt.hash(password, 10);
+        this.password = await brypt.hash(this.password, 10);
+        console.log(this.password);
+
+        this.confirmpassword = undefined;
+
+    }
+    next();
+
+
+} )
 
 // collection
  const Register = new mongoose.model("Register",regSchema)
