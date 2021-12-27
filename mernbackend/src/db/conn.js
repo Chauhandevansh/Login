9b743fbdb7f3b8811a4c7bb843c7f9cd73d8d213
Chauhandevansh <69097279+Chauhandevansh@users.noreply.github.com>
2021-12-27 16:27:42 +0530

    Update conn.js

--- mernbackend/src/db/conn.js
+++ mernbackend/src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/youtubeReg",{
+mongoose.connect("mongodb+srv://devansh:devdev30@cluster0.nivlp.mongodb.net/test",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
